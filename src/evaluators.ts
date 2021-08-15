634d60194ecb986d7f4e74b5d79b8b9ca618b851
Adwin Ying <adwinying@gmail.com>
2021-08-15 13:12:22 +0900

    Remove internal keys when evaluating expression

--- src/evaluators.ts
+++ src/evaluators.ts
@@ -1,7 +1,11 @@
 export const evaluate = (expression: string, ctx: Record<string, any>) => {
+  // _slot should not be part of the evaluation context hence it is removed
+  const evalCtx = { ...ctx };
+  delete evalCtx._slot;
+
   // eslint-disable-next-line @typescript-eslint/no-implied-eval
   const func = new Function(`
-    with (${JSON.stringify(ctx)}) return (${expression})
+    with (${JSON.stringify(evalCtx)}) return (${expression})
   `);
 
   let result = null;
